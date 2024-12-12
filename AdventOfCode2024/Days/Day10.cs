d6f802e38a07188984cf9bc9f5351bbec4ee226d
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2024-12-12 07:47:15 +0100

    day 10

---
+++ AdventOfCode2024/Days/Day10.cs
@@ -0,0 +1,93 @@
+﻿namespace AdventOfCode2024.Days;
+
+public class Day10(IInputAgent agent, IResultPrinter resultPrinter) : IDay
+{
+    public async Task Part1()
+    {
+        var input = (await agent.GetInputLines(10)).Select(line =>
+            line.ToCharArray().Select(c => Convert.ToInt32(char.GetNumericValue(c))).ToArray()).ToArray();
+        var sum = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] != 0) continue;
+
+                sum += CheckTile(input, j, i).Distinct().Count();
+            }
+        }
+
+        resultPrinter.Print(10, 1, sum);
+    }
+
+    public static List<Tuple<int,int>> CheckTile(int[][] field, int x, int y)
+    {
+        int currentValue = field[y][x];
+
+        if (currentValue == 9)
+        {
+            Console.WriteLine($"found a 9 at y={y} x={x}");
+            return [new Tuple<int, int>(x,y)];
+        }
+
+        List<Tuple<int,int>> pathCount = [];
+
+        // check up down left right
+        if (y > 0)
+        {
+            int upValue = field[y - 1][x];
+            if (upValue == currentValue + 1)
+            {
+                pathCount.AddRange(CheckTile(field, x, y - 1));
+            }
+        }
+
+        if (y < field.Length - 1)
+        {
+            int downValue = field[y + 1][x];
+            if (downValue == currentValue + 1)
+            {
+                pathCount.AddRange(CheckTile(field, x, y + 1));
+            }
+        }
+
+        if (x > 0)
+        {
+            int leftValue = field[y][x - 1];
+            if (leftValue == currentValue + 1)
+            {
+                pathCount.AddRange(CheckTile(field, x - 1, y));
+            }
+        }
+
+        if (x < field[y].Length - 1)
+        {
+            int rightValue = field[y][x + 1];
+            if (rightValue == currentValue + 1)
+            {
+                pathCount.AddRange(CheckTile(field, x + 1, y));
+            }
+        }
+
+        return pathCount;
+    }
+
+    public async Task Part2()
+    {
+        var input = (await agent.GetInputLines(10)).Select(line =>
+            line.ToCharArray().Select(c => Convert.ToInt32(char.GetNumericValue(c))).ToArray()).ToArray();
+        var sum = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] != 0) continue;
+
+                sum += CheckTile(input, j, i).Count;
+            }
+        }
+
+        resultPrinter.Print(10, 2, sum);
+
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -10,15 +10,8 @@ using Microsoft.Extensions.Logging;
 
 Env.Load();
 
-IInputAgent inputAgent = new InputAgent(new HttpClient());
-IResultPrinter resultPrinter = new ResultPrinter();
-Day1 day1 = new Day1(inputAgent, resultPrinter);
-for (int i = 0; i < 10000; i++)
-{
-await day1.Part1();
-}
-return;
-
+// get the day input parameter supplied by the the launch command
+int? dayArg = args.Length > 0 ? int.Parse(args[0]) : null;
 
 var serviceCollection = new ServiceCollection();
 
@@ -45,33 +38,55 @@ serviceCollection.AddScoped<IResultPrinter, ResultPrinter>();
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
-
-// Use reflection to get all classes implementing IDay, Expect them to have the name Day<num> where num is a 1 or two digit number. Sort by the numeric value of that number. Then run Part1 and Part2 in order
-IEnumerable<IDay?> days = Assembly.GetExecutingAssembly().GetTypes()
-    .Where(t => t.GetInterfaces().Contains(typeof(IDay)))
-    .OrderBy(t => int.Parse(Regex.Match(t.Name, @"\d+").Value))
-    .Select(t => ActivatorUtilities.CreateInstance(serviceProvider, t) as IDay);
-
-ILogger logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-
-var stopwatch = new Stopwatch();
-var intervalStopwatch = new Stopwatch();
-
-stopwatch.Start();
-foreach (IDay? day in days)
+if (dayArg == null)
 {
-    intervalStopwatch.Start();
-    await day.Part1();
-    intervalStopwatch.Stop();
-    logger.LogDebug($"Day {day.GetType().Name} Part 1 took {intervalStopwatch.ElapsedMilliseconds}ms");
-    intervalStopwatch.Reset();
+    // get class implementingIday containing the dayArg
+    var dayType = Assembly.GetExecutingAssembly().GetTypes()
+        .Where(t => t.GetInterfaces().Contains(typeof(IDay)))
+        .FirstOrDefault(t => t.Name == $"Day{DateTime.Now.Day}");
 
-    intervalStopwatch.Start();
+    if (dayType == null)
+    {
+        Console.WriteLine($"Day {DateTime.Now.Day} not found");
+        return;
+    }
+
+    var day = ActivatorUtilities.CreateInstance(serviceProvider, dayType) as IDay;
+    await day.Part1();
     await day.Part2();
-    intervalStopwatch.Stop();
-    logger.LogDebug($"Day {day.GetType().Name} Part 2 took {intervalStopwatch.ElapsedMilliseconds}ms");
-    intervalStopwatch.Reset();
+    return;
 }
 
-stopwatch.Stop();
-logger.LogInformation($"All days took {stopwatch.ElapsedMilliseconds}ms");
+if (dayArg == 0)
+{
+// Use reflection to get all classes implementing IDay, Expect them to have the name Day<num> where num is a 1 or two digit number. Sort by the numeric value of that number. Then run Part1 and Part2 in order
+    IEnumerable<IDay?> days = Assembly.GetExecutingAssembly().GetTypes()
+        .Where(t => t.GetInterfaces().Contains(typeof(IDay)))
+        .OrderBy(t => int.Parse(Regex.Match(t.Name, @"\d+").Value))
+        .Select(t => ActivatorUtilities.CreateInstance(serviceProvider, t) as IDay);
+
+    foreach (IDay? day in days)
+    {
+        await day.Part1();
+        await day.Part2();
+    }
+
+    return;
+}
+else
+{
+    // get class implementing Iday containing the dayArg
+    var dayType = Assembly.GetExecutingAssembly().GetTypes()
+        .Where(t => t.GetInterfaces().Contains(typeof(IDay)))
+        .FirstOrDefault(t => t.Name == $"Day{dayArg}");
+
+    if (dayType == null)
+    {
+        Console.WriteLine($"Day {dayArg} not found");
+        return;
+    }
+
+    var day = ActivatorUtilities.CreateInstance(serviceProvider, dayType) as IDay;
+    await day.Part1();
+    await day.Part2();
+}
